9e90973572d51ed86f08ca2168d243d9a963ff96
delta-dev1 <38355321+delta-dev1@users.noreply.github.com>
2018-05-14 14:28:57 +0200

    Fixed default route
    Fixed default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
-import { HomeComponent } from './core/home/home.component';
 import { EmployeesComponent } from './core/employees/employees.component';
 import { ProfileComponent } from './core/profile/profile.component';
 import { StatsComponent } from './core/stats/stats.component';
+import { SigninComponent } from './auth/signin/signin.component';
 
 const appRoutes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: '', redirectTo: '/signin', pathMatch: 'full' },
+    { path: 'signin', component: SigninComponent },
     { path: 'employees', component: EmployeesComponent},
     { path: 'profile', component: ProfileComponent},
     { path: 'stats', component: StatsComponent}
